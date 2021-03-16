f01e6ba1f7244a681d80eb1013d3631c8a906459
Bradley Kang <bkang@lcs.on.ca>
2021-03-16 14:07:32 -0400

    Initial Commit

---
+++ CulminatingTask/CulminatingTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CulminatingTaskApp.swift
+//  CulminatingTask
+//
+//  Created by Brad Kang on 2021-03-16.
+//
+
+import SwiftUI
+
+@main
+struct CulminatingTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
