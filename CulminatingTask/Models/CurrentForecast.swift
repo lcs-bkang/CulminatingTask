1efefef7e2fb4226162500b08c5774a2cf63e67e
Bradley Kang <bkang@lcs.on.ca>
2021-03-18 09:28:27 -0400

    Added the current forecast structure

--- CulminatingTask/Models/CurrentForecast.swift
+++ CulminatingTask/Models/CurrentForecast.swift
@@ -7,4 +7,14 @@
 
 import Foundation
 
-
+struct CurrentForecast: Decodable, Identifiable {
+    
+    var id: Int
+    var temp_c: Double
+    var wind_kph: Double
+    var wind_dir: Double
+    var precip_mm: Double
+    var humidity: Int
+    var visib_km: Double
+    var uv: Double
+}
