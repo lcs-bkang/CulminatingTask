5ef466d9d8b82e6ac14138badccdfde593ac8d82
Bradley Kang <bkang@lcs.on.ca>
2021-03-24 21:24:38 -0400

    Abstracted out the two halves of the Calendar view

---
+++ CulminatingTask/Views/Calendar/CalendarTopHalf.swift
@@ -0,0 +1,35 @@
+//
+//  CalendarTopHalf.swift
+//  CulminatingTask
+//
+//  Created by Brad Kang on 2021-03-24.
+//
+
+import SwiftUI
+
+struct CalendarTopHalf: View {
+    
+    // Variable to hold the month
+    @State var month: String = "March"
+    
+    // Variable to hold the year
+    @State var year: String = "2021"
+    
+    var body: some View {
+        VStack {
+                Rectangle()
+                .fill(Color.black)
+                .frame(width: 320, height: 1)
+            HStack {
+                Text("\(month) \(year)")
+                    .padding(.leading, 15.0)
+                Spacer()
+            }
+    }
+}
+}
+struct CalendarTopHalf_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarTopHalf()
+    }
+}
