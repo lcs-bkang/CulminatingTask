b79f8b97c50cffd75d95dee6cc374c3fa9290a0b
Bradley Kang <bkang@lcs.on.ca>
2021-03-24 21:32:18 -0400

    Removed the green background

---
+++ CulminatingTask/Views/Calendar/CalendarBottomHalf.swift
@@ -0,0 +1,402 @@
+//
+//  CalendarBottomHalf.swift
+//  CulminatingTask
+//
+//  Created by Brad Kang on 2021-03-24.
+//
+
+import SwiftUI
+
+struct CalendarBottomHalf: View {
+    var body: some View {
+
+        HStack {
+            Text("S")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+            Text("M")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+            Text("T")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+            Text("W")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+            Text("T")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+            Text("F")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+            Text("S")
+                .frame(width: 43, height: 55)
+                .padding(.all, -5.0)
+        }
+        HStack {
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("28")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("1")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("2")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("3")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("4")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("5")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("6")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+
+        }
+        .padding(.top, -15.0)
+        HStack {
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("7")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("8")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("9")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("10")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("11")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("12")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("13")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+
+        }
+        HStack {
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("14")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("15")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("16")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("17")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("18")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("19")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("20")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+
+        }
+        HStack {
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("21")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("22")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("23")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+                    .border(Color(.black))
+                Text("24")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("25")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("26")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("27")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+
+        }
+        HStack {
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("28")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("29")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("30")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("31")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("1")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("2")
+                    .foregroundColor(.black)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+            ZStack {
+                Rectangle()
+                    .frame(width: 43, height: 55)
+
+                    .border(Color(.black))
+                Text("3")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+            }
+            .padding(.all, -5.0)
+
+        }
+        Spacer()
+    }
+
+}
+
+struct CalendarBottomHalf_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarBottomHalf()
+    }
+}
